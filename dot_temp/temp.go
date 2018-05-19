74eed3be32ecb3f9ce0704bd31b2258ba0f7a84f
Yuri <erokhin93@yandex.ru>
2018-05-19 16:30:13 +0500

    Добавлен файл с шаблонами (новость, карта сайта, и т.д.)

---
+++ dot_temp/temp.go
@@ -0,0 +1,33 @@
+package dot_temp
+
+/*
+ * Шаблон новости
+ */
+type TempNews struct {
+	Link string
+	Header string
+	Description string
+}
+
+/*
+ * Бокс для новостей
+ */
+
+type TempSetNews struct {
+	SetNews []TempNews
+}
+
+/*
+ * Шаблон карты сайта
+ */
+type TempSitemap struct {
+	Sitemap []string `xml:"sitemap>loc"`
+}
+
+/*
+ * Дочерние элементы в sitemap
+ */
+
+type TempSitemapUrls struct {
+	SitemapUrls []string `xml:"url>loc"`
+} 
