d9253b0575b25e096a3fe6e177f2709e6bb41cc1
Yuri <erokhin93@yandex.ru>
2018-05-20 18:16:39 +0500

    Добавлен пакет dot_db для работа с запросами к базе данных MySQL

--- dot_db/database.go
+++ dot_db/database.go
@@ -1,1 +1,30 @@
 package dot_db
+
+import (
+	"database/sql"
+	"fmt"
+	"dotBankNewsAggregator/dot_temp"
+)
+
+type NewItem struct {
+	DB *sql.DB
+	Note dot_temp.TempNews
+}
+
+func Insert(post NewItem){
+	result, err := post.DB.Exec(
+		"insert into dot_news.news (site_url, post_link, post_header, post_description) values (?, ?, ?, ?)",
+		post.Note.Site,
+		post.Note.Link,
+		post.Note.Header,
+		post.Note.Description)
+
+	if err != nil{
+		panic(err)
+	}
+
+	//ID добавленного объекта
+	fmt.Println(result.LastInsertId())
+	//Количество добавленных строк
+	fmt.Println(result.RowsAffected())
+}
