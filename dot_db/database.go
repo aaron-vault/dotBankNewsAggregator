c164c423ecac0e8051b016b517e0991fc9da202c
Yuri(work) <erokhin93@yandex.ru>
2018-05-21 11:15:41 +0500

    Добавлен файл database.go для работы с базой данных

--- dot_db/database.go
+++ dot_db/database.go
@@ -4,11 +4,17 @@ import (
 	"database/sql"
 	"fmt"
 	"dotBankNewsAggregator/dot_temp"
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type NewItem struct {
 	DB *sql.DB
 	Note dot_temp.TempNews
+}
+
+type MysqlTempNews struct {
+	ID string
+	Arr dot_temp.TempNews
 }
 
 func Insert(post NewItem){
@@ -27,4 +33,24 @@ func Insert(post NewItem){
 	fmt.Println(result.LastInsertId())
 	//Количество добавленных строк
 	fmt.Println(result.RowsAffected())
+}
+
+func Select(table string, db sql.DB) dot_temp.TempSetNews  {
+	rows, err := db.Query("select * from news")
+	if err != nil{
+		panic(err)
+	}
+	defer rows.Close()
+	data := dot_temp.TempSetNews{}
+
+	for rows.Next() {
+		obj := MysqlTempNews{}
+		err := rows.Scan(&obj.ID, &obj.Arr.Site, &obj.Arr.Link, &obj.Arr.Header, &obj.Arr.Description)
+		if err != nil{
+			fmt.Println(err)
+			continue
+		}
+		data.SetNews = append(data.SetNews, obj.Arr)
+	}
+	return data
 }
